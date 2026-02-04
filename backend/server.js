8354826ae6341e28729d8b1ba312f1c3da0eac91
jai099 <131276939+jai099@users.noreply.github.com>
2026-02-04 14:02:13 +0530

    Admin login and user data fetched in the admin dashbaord

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
+
 dotenv.config();
 
 const voterRoutes = require("./routes/voterRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
 
@@ -13,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/voters", voterRoutes);
+app.use("/api/admin", adminRoutes);
 
 app.get("/", (req, res) => res.send("API Running"));
 
